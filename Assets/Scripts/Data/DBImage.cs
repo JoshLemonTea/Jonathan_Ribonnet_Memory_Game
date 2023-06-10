313d21330d395ada6fe4eb623bc56555f77ffcf9
JoshLemonTea <jonathan.ribonnet@student.howest.be>
2023-06-10 19:04:13 +0200

    More explainations

--- Assets/Scripts/Data/DBImage.cs
+++ Assets/Scripts/Data/DBImage.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace Memory.Data
 {
+    // Define a class called DBImage
     class DBImage
     {
+        // Define public properties for the DBImage class
         public int Id { get; set; }
         public string Name { get; set; }
         public byte[] MemoryImage { get; set; }
 
-        //public bool IsBack = false;
+        // Uncommented code line, which is a public field declaration
+        // public bool IsBack = false;
     }
 }
--- Assets/Scripts/Data/Singleton.cs
+++ Assets/Scripts/Data/Singleton.cs
@@ -7,19 +7,29 @@ using UnityEngine;
 
 namespace Memory.Data
 {
+    // Define a generic abstract class Singleton<T> that inherits from MonoBehaviour
     public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
+        // Create a Lazy<T> instance that will hold the singleton instance
         private static readonly Lazy<T> LazyInstance = new Lazy<T>(CreateSingleton);
 
+        // Define a public static property 'Instance' that returns the singleton instance
         public static T Instance => LazyInstance.Value;
 
+        // Define a private static method 'CreateSingleton' that creates and returns the singleton instance
         private static T CreateSingleton()
         {
+            // Create a new GameObject to hold the singleton instance, with a name based on the type of T
             var ownerObject = new GameObject($"{typeof(T).Name} (singleton)");
+
+            // Add an instance of T as a component to the ownerObject
             var instance = ownerObject.AddComponent<T>();
+
+            // Prevent the ownerObject from being destroyed when a new scene is loaded
             DontDestroyOnLoad(ownerObject);
+
+            // Return the created instance
             return instance;
         }
     }
-
 }
